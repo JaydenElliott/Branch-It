83bcb84b63916da06ce8590e100b7aae7200071d
JaydenElliott <jayden.elliott@outlook.com>
2020-12-01 15:23:26 +1100

    BUILD: homescreen log-in modal added

--- treevy_client/src/components/homePage.tsx
+++ treevy_client/src/components/homePage.tsx
@@ -1,12 +1,26 @@
+// Project Imports
 import React, { ChangeEvent, Component } from "react";
 import "../styles/page-styles/homePage.css";
+
+// Button & Icons
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import ShareIcon from "@material-ui/icons/Share";
 import PersonIcon from "@material-ui/icons/Person";
 import EcoIcon from "@material-ui/icons/Eco";
 import logo from "../logo/templogo.svg";
-import TextField from '@material-ui/core/TextField';
+
+// Search Bar
+import TextField from "@material-ui/core/TextField";
+
+// Log-in Dialog (modal)
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+//
 
 // Defines the layout pallete for the different UI elements.
 const pallete = {
@@ -20,12 +34,13 @@ const pallete = {
   buttonVariant: "contained",
 
   // Premium pallete
-  permiumButtonColour: "#2196f3"
-}
+  permiumButtonColour: "#2196f3",
+};
 
 interface HomePageState {
-  sString: string,  // Search string
-  displayedToDoLists: string[] // To-do lists displayed to the user according to the search
+  sString: string; // Search string
+  displayedToDoLists: string[]; // To-do lists displayed to the user according to the search
+  modalOpen: boolean;
 }
 
 export default class HomePage extends Component<any, HomePageState> {
@@ -34,11 +49,15 @@ export default class HomePage extends Component<any, HomePageState> {
 
     this.state = {
       sString: "",
-      displayedToDoLists: this.props.toDoLists
-    }
+      displayedToDoLists: this.props.toDoLists,
+      modalOpen: false,
+    };
   }
 
   /**
+   *
+   * FUNCTIONALITY: SEARCH BAR
+   *
    * Changes the provided search contents to represent the search.
    * @param e current input
    */
@@ -47,9 +66,9 @@ export default class HomePage extends Component<any, HomePageState> {
     if (this.state.displayedToDoLists === undefined) return;
 
     // Finds all lists containing the searched word
-    let newDisplayedList : string[] = [];
-    const currentSearch : string = e.currentTarget.value;
-    this.props.toDoLists.forEach((toDo : string) => {
+    let newDisplayedList: string[] = [];
+    const currentSearch: string = e.currentTarget.value;
+    this.props.toDoLists.forEach((toDo: string) => {
       // To ensure that the search is not case sensitive, both are set to lower case.
       if (toDo.toLowerCase().includes(currentSearch.toLowerCase())) {
         newDisplayedList.push(toDo);
@@ -58,44 +77,71 @@ export default class HomePage extends Component<any, HomePageState> {
 
     // Sets new displayed lists
     this.setState({
-      displayedToDoLists: newDisplayedList
-    })
-  }
+      displayedToDoLists: newDisplayedList,
+    });
+  };
 
   /**
+   *
+   * FUNCTIONALITY: SEARCH BAR
+   *
    * Displays the user's to-do lists given the current search input.
    */
-  displayToDoLists = () : JSX.Element | void => {
+  displayToDoLists = (): JSX.Element | void => {
     // If the to-do list is not provided, do nothing.
     if (this.state.displayedToDoLists === undefined) return;
 
     return (
       <div>
-        {this.state.displayedToDoLists.map(list => <div>{list}</div>)}
+        {this.state.displayedToDoLists.map((list) => (
+          <div>{list}</div>
+        ))}
       </div>
     );
-  }
+  };
+
+  /**
+   *
+   * FUNCTIONALITY: LOG-IN BUTTON - Modal
+   *
+   * Modal open and close state manipulation
+   */
+  modalClickOpen = () => {
+    this.setState({
+      modalOpen: true,
+    });
+  };
+  modalClickClose = () => {
+    this.setState({
+      modalOpen: false,
+    });
+  };
 
   render() {
     return (
       <div className="grid-container">
         <div className="topbar-container">
           {/* Search bar */}
-          <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+          <form
+            autoComplete="off"
+            onSubmit={(e) => e.preventDefault()}
+            style={{ height: "100%" }}
+          >
             <TextField
               id="search-bar"
               placeholder="Search"
               variant="filled"
-              size="medium"
+              size="small"
+              InputLabelProps={{ shrink: true }}
               inputProps={{
                 style: {
                   fontSize: "large",
-                  fontFamily: "handWritten"
+                  fontFamily: "handWritten",
                 },
               }}
               onChange={this.onSearchChange}
-              style={{width: "221%"}}>
-              </TextField>
+              style={{ width: "221%", minHeight: "10px" }}
+            ></TextField>
           </form>
 
           <div className="top-logo">
@@ -146,6 +192,7 @@ export default class HomePage extends Component<any, HomePageState> {
           <div className="Log-in">
             <Button
               startIcon={<PersonIcon />}
+              onClick={this.modalClickOpen}
               variant="contained"
               style={{
                 backgroundColor: "#608C4C",
@@ -155,6 +202,49 @@ export default class HomePage extends Component<any, HomePageState> {
             >
               Log-in
             </Button>
+            <div style={{ display: "flex", alignItems: "flex-end" }}>
+              <Dialog
+                open={this.state.modalOpen}
+                onClose={this.modalClickClose}
+                aria-labelledby="form-dialog-title"
+              >
+                <DialogTitle id="form-dialog-title">
+                  <span style={{ fontSize: "25px" }}>Welcome Back</span>
+                </DialogTitle>
+                <DialogContent>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    inputProps={{
+                      style: {
+                        fontSize: "large",
+                      },
+                    }}
+                  />
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    inputProps={{
+                      style: {
+                        fontSize: "large",
+                      },
+                    }}
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={this.modalClickClose}>Cancel</Button>
+                  <Button onClick={this.modalClickClose}>Log-in</Button>
+                </DialogActions>
+              </Dialog>
+            </div>
           </div>
         </div>
 
--- treevy_client/src/components/testingGrounds.tsx
+++ treevy_client/src/components/testingGrounds.tsx
@@ -6,55 +6,10 @@ import PersonIcon from "@material-ui/icons/Person";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
 export default class TestingGrounds extends Component<any, any> {
   render() {
-    return (
-      <div>
-        <div>
-          <Button
-            startIcon={<SaveIcon />}
-            variant="contained"
-            style={{ backgroundColor: "#4db6ac" }}
-          >
-            Save
-          </Button>
-        </div>
-        <div style={{ marginTop: "50px" }}>
-          <Button
-            startIcon={<ShareIcon />}
-            variant="contained"
-            style={{ backgroundColor: "#4db6ac" }}
-          >
-            Share
-          </Button>
-        </div>
-        <div style={{ marginTop: "50px" }}>
-          <Button
-            startIcon={<PersonIcon />}
-            variant="contained"
-            style={{ backgroundColor: "#bfe7dc" }}
-          >
-            Log in
-          </Button>
-        </div>
-        <div style={{ marginTop: "50px" }}>
-          <Button
-            startIcon={<FacebookIcon />}
-            variant="contained"
-            style={{
-              backgroundColor: "#5e77f9",
-              color: "#ffffff",
-            }}
-          >
-            Sign up with Facebook
-          </Button>
-        </div>
-
-        <FormControlLabel
-          control={<Checkbox name="checkedB" color="primary" />}
-          label="Primary"
-        />
-      </div>
-    );
+    return <div></div>;
   }
 }
