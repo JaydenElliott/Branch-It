e1b8b0b7b7ff19736440a03cfeea30394904bba8
Ethal Askander <Ethal.Askander@outlook.com>
2020-12-05 19:06:42 +1100

    REFACTOR: #33 the search bar and side bar container have been put together a single react class component

--- treevy_client/src/App.tsx
+++ treevy_client/src/App.tsx
@@ -1,32 +1,9 @@
 import React, { ChangeEvent } from "react";
 import { Component } from "react";
 import "./App.css";
-import ListBoard from "./components/listHandling/listBoard";
-import ListHandler from "./components/listHandling/listHandler";
 import HomePage from "./components/homePage/homePage";
-import SignUpPage from "./components/homePage/sign-up/signupPage";
-import TestingGrounds from "./components/testing/testingGrounds";
-import AccountButton from "./components/homePage/log-in/accountButton";
-import LogInButton from "./components/homePage/log-in/loginButton";
 
 import axios from "axios";
-
-/**
- * NOTE!!!!
- *
- * DO NOT NEED LOCATION item number if assume that there can only be one root for tree.
- *  Tree rendering would be based off one list. Suppose list name was TODO, each "root"
- *  of that would just be a child node of TODO
- *
- * Potential in future for rendering all your trees in one diagram.
- */
-const temptodo = [
-  "Frontend",
-  "Backend",
-  "My lists button",
-  "Shared lists button",
-  "Fixing the search bar grid...",
-];
 
 class App extends Component<{}, any> {
   constructor(props: any) {
@@ -47,7 +24,7 @@ class App extends Component<{}, any> {
     return (
       <div className="maindiv">
         {/* <TestingGrounds /> */}
-        <HomePage toDoLists={temptodo} />
+        <HomePage />
         {/* <ListHandler /> */}
         {/* <SignUpPage /> */}
       </div>
