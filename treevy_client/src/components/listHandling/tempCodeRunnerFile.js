84e482ab6ebb98561beaa99bfa4f69513e30f701
JaydenElliott <jayden.elliott@outlook.com>
2020-12-18 14:01:19 +1100

    BUILD: coord generator

---
+++ treevy_client/src/components/listHandling/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log(
+  coord_adapter(root_coordinate, node3.compute_coordinate(), xscale, yscale)
+);
---
+++ treevy_client/src/components/listHandling/toDel.tsx
@@ -0,0 +1,18 @@
+import React, { ChangeEvent, Component } from "react";
+import Node from "./newList";
+
+var WIDTH = 2;
+let root_coordinate = [1000, 100];
+let xscale = 20;
+let yscale = 50;
+
+export default class RenderCoords extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
