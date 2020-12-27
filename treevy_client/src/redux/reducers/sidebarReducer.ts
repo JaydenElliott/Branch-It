75cef2a5f154fad517567aff18efa4066c72c1be
Ethal Askander <Ethal.Askander@outlook.com>
2020-12-27 20:47:33 +1100

    Fixed file names

---
+++ treevy_client/src/redux/reducers/sidebarReducer.ts
@@ -0,0 +1,20 @@
+// Defines the redux reducer for the sidebar
+const initialState = {
+    width: 0,
+}
+const sidebarReducer = (state = initialState, action: any) => {
+    // Reducer actions
+    switch(action.type) {
+        case 'sidebar/resize':
+            state = {
+                ...state,
+                width: action.payload,
+            }
+            break;
+        default:
+            break;
+    }
+    return state;
+};
+
+export default sidebarReducer;
