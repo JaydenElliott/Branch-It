738c6915397a89fa73df7d298d2a928c9010f7a8
JaydenElliott <jayden.elliott@outlook.com>
2020-12-27 12:22:01 +1100

    BUILD: media queries for smaller screens

--- treevy_client/src/components/homePage/top-bar/share/shareButton.tsx
+++ treevy_client/src/components/homePage/top-bar/share/shareButton.tsx
@@ -2,8 +2,6 @@ import React, { ChangeEvent, Component } from "react";
 import Button from "@material-ui/core/Button";
 import ShareIcon from "@material-ui/icons/Share";
 
-import "../../../../componentStyles/homePage/top-bar/share/shareButton.css";
-
 export default class ShareButton extends Component<any, any> {
   constructor(props: any) {
     super(props);
@@ -11,20 +9,18 @@ export default class ShareButton extends Component<any, any> {
 
   render() {
     return (
-      <div className="top-bar-column-3-share">
-        <Button
-          startIcon={<ShareIcon />}
-          variant="contained"
-          style={{
-            backgroundColor: "#608C4C",
-            color: "#ffffff",
-            height: "33px",
-            width: "95px",
-          }}
-        >
-          Share
-        </Button>
-      </div>
+      <Button
+        startIcon={<ShareIcon />}
+        variant="contained"
+        style={{
+          backgroundColor: "#608C4C",
+          color: "#ffffff",
+          height: "33px",
+          width: "95px",
+        }}
+      >
+        Share
+      </Button>
     );
   }
 }
