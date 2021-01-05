0a76569a7f9e2987f50dd111fa341650b17f751e
Ethal <u6669384@anu.edu.au>
2021-01-05 13:12:07 +1100

    BUILD: #74 Now able to add a new list to MySQL using backend flask api

--- treevy_backend/src/backend/flask_server_play/app.py
+++ treevy_backend/src/backend/flask_server_play/app.py
@@ -12,62 +12,73 @@ CORS(api)
 # Setting up MySQLCommunicator
 communicator : MySQLCommunicator = MySQLCommunicator()
 
-@api.route('/lists', methods=['GET'])
+@api.route('/lists', methods=['GET', 'POST'])
 def get_lists():
-    '''
-    @params e: email.
 
-    Returns all lists associated with user
-    '''
-    if request.method == 'GET' and 'e' in request.args:
-        email = request.args['e']
-        usr_id = communicator.get_user_id(email)
-        if (usr_id == None):
-            # Nothing was retrieved form the database for the given parameters. Status code: not found (404)
-            return "Error: email does not exist", 404
-        
-        res = communicator.get_lists(usr_id)
+    # Get request
+    if request.method == 'GET':
+        '''
+        @params e: email.
 
-        if (res == None):
-            # Nothing was retrieved form the database for the given parameters. Status code: not found (404)
-            return "Error: no lists found for user", 404
-        else:
-            # FIX: return a json which has appropriate key strings (currently are just numbers: 1, 2, etc.).
-            # lists = []
-            # for list in res:
-            #     lists.append(json.dumps({ # Dumps simply outputs a string from this.
-            #         'list_id':list[0],
-            #         'user_id':list[1],
-            #         'list':list[2]
-            #     }))
-            # print(lists)
-            return jsonify(res), 200
-    else:
-        # No email provided, status code: bad request (400)
-        return "Error: no email provided", 400
-
-# @api.route('lists', method=['POST'])
-# def add_list():
-#     '''
-#     @params e: email and l: json
-
-#     Returns id of newly added list
-#     '''
-
-#     # First checks if email parameter is provided
-#     if request.method == 'POST' and 'e' in request.args:
-#         email = request.args['e']
-#         usr_id = communicator.get_user_id(email)
-#         if (usr_id == None):
-#             return "Error: email does not exist", 404
-        
-#         # Then checks if list is provided
-#         if 'l' in request.args:
+        Returns all lists associated with user
+        '''
+        if 'e' in request.args:
+            email = request.args['e']
+            usr_id = communicator.get_user_id(email)
+            if (usr_id == None):
+                # Nothing was retrieved form the database for the given parameters. Status code: not found (404)
+                return "Error: email does not exist", 404
             
-#         else:
-#             return "Error: no list provided", 400
-#     else:
-#         return "Error: no email provided", 400
+            res = communicator.get_lists(usr_id)
+
+            if (res == None):
+                # Nothing was retrieved form the database for the given parameters. Status code: not found (404)
+                return "Error: no lists found for user", 404
+            else:
+                # FIX: return a json which has appropriate key strings (currently are just numbers: 1, 2, etc.).
+                # lists = []
+                # for list in res:
+                #     lists.append(json.dumps({ # Dumps simply outputs a string from this.
+                #         'list_id':list[0],
+                #         'user_id':list[1],
+                #         'list':list[2]
+                #     }))
+                # print(lists)
+                return jsonify(res), 200
+        else:
+            # No email provided, status code: bad request (400)
+            return "Error: no email provided", 400
+    
+    # POST request
+    elif request.method == 'POST':
+        '''
+        @data json: {email: string, list: json}.
+
+        Returns id of newly added list.
+        Warning: will not check if list structure or contents.
+        '''
+
+        data = request.get_json()
+        if (data == None):
+            return "Error: nothing provided", 400
+        
+        # First checks if email parameter is provided
+        if 'email' in data:
+            # Obtain user_id from email
+            email = data['email']
+            usr_id = communicator.get_user_id(email)
+            if (usr_id == None):
+                return "Error: email does not exist", 404
+            
+            # Then checks if list is provided
+            if 'list' in data:
+                # Insert the list
+                list_id = communicator.insert_treevy(usr_id, data['list'])
+                return str(list_id), 200
+            else:
+                return "Error: no list provided", 400
+        else:
+            return "Error: no email provided", 400
 
 
 @api.route("/users", methods=['GET'])
@@ -108,7 +119,9 @@ def login():
     Returns user details excluding the password if successful
     '''
     data = request.get_json()
-
+    if (data == None):
+            return "Error: nothing provided", 400
+    
     # Check that the relevant data was provided
     if ('email' in data and 'password' in data):
         res = communicator.get_user_details_from_email(data['email'])
