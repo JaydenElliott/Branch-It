57f9f01c5b596978359f844efb192abedfefac6c
Ethal Askander <Ethal.Askander@outlook.com>
2021-02-06 10:24:57 +1100

    BUILD: Added coordinates to todoList

--- front-end/src/Components/list-handling/todoList.js
+++ front-end/src/Components/list-handling/todoList.js
@@ -6,10 +6,12 @@ export default class TodoList {
   done = false;
   listName = "";
   children = [];
+  coordinates = {x: 0, y: 0};
   
-  constructor(listName, done = false, children = []) {
+  constructor(listName, done = false, children = [], coordinates = {x: 0, y: 0}) {
     this.listName = listName;
     this.done = done;
     this.children = children;
+    this.coordinates = coordinates;
   }
 }
