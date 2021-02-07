eb7515205fb5cce17a7f5dadf5669de0425b2343
JaydenElliott <jayden.elliott@outlook.com>
2021-02-07 16:56:43 +1100

    BUILD: basic modal form.

--- front-end/src/Components/home-page/content-container/list-nav/list/dotPointList.js
+++ front-end/src/Components/home-page/content-container/list-nav/list/dotPointList.js
@@ -5,18 +5,77 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 import "./dotPointList.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 class DotPointList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       itemModalOpen: false,
+      addListInput: "",
     };
   }
 
-  addChildList = () => {
-    this.props.list.addList(new TodoList("testingName"));
-    this.forceUpdate();
+  addChildList = (e) => {
+    e.preventDefault();
+    this.props.list.addList(new TodoList(this.state.addListInput));
+
+    // Reset input
+    this.setState({
+      addListInput: "",
+    });
+  };
+
+  modalSwitch = () => {
+    this.setState({
+      itemModalOpen: !this.state.itemModalOpen,
+    });
+  };
+
+  onInputChange = (e) => {
+    this.setState({
+      addListInput: e.currentTarget.value,
+    });
+  };
+
+  renderItemModal = () => {
+    return (
+      <div className="item-modal-container">
+        <button className="modal-item-close" onClick={this.modalSwitch}>
+          &times;
+        </button>
+        <div className="item-modal-title">{this.props.list.listName}</div>
+        <div className="item-modal-content">
+          <div className="item-modal-content-title">Description:</div>
+          <div className="item-modal-description"></div>
+          <div className="item-modal-content-title">Add Child List:</div>
+          <div className="item-modal-add-child">
+            <form onSubmit={this.addChildList}>
+              <input
+                onChange={this.onInputChange}
+                value={this.state.addListInput}
+              />
+              <button type="submit">
+                <FontAwesomeIcon
+                  icon={faPlus}
+                  style={{ height: "100%", width: "100%" }}
+                />
+              </button>
+            </form>
+          </div>
+          <div className="item-modal-remove-list">
+            <div className="item-modal-content-title">Remove List</div>{" "}
+            <button>
+              <FontAwesomeIcon
+                icon={faMinus}
+                style={{ height: "100%", width: "100%" }}
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   };
 
   render() {
@@ -26,8 +85,9 @@ class DotPointList extends Component {
           <div className="dot-point-dot">&#183;</div>
           <div className="dot-point-title">{this.props.list.listName}</div>
           <div className="dot-point-more">
-            <button onClick={this.addChildList}>Add</button>
+            <button onClick={this.modalSwitch}>Add</button>
           </div>
+          {this.state.itemModalOpen && this.renderItemModal()}
         </div>
         {this.props.list.children.map((todo) => {
           return <DotPointList list={todo} depth={this.props.depth + 1} />;
